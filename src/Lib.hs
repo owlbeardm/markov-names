a9f7667bcf71eb39514c2f42af499257cfd81655
Mikita Kukavenka <godinfrog@gmail.com>
2021-07-21 18:33:05 +0300

    mb

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,12 +2,17 @@ module Lib
   ( someFunc
   ) where
 
-import           ClassyPrelude (Char, Double, IO, Integer, Text, fromIntegral,
-                                length, map, putStrLn, sum, toInteger, (*), (+),
-                                (-), (.), (/), (<), (<=), (==), (>), (^))
-import           Data.List     (nub)
-import qualified Data.Text     as DT (snoc, tail, take, unpack)
-import           Markov.Count  (count)
+import           ClassyPrelude          (Char, Double, IO, Integer, Show, Text,
+                                         fromIntegral, length, map, putStrLn,
+                                         show, sum, toInteger, unzip4, (*), (+),
+                                         (-), (.), (/), (<), (<=), (==), (>),
+                                         (^))
+import           Data.List              (nub)
+import qualified Data.Text              as DT (head, singleton, snoc, tail,
+                                               take, unpack)
+import           Markov.Count           (count)
+import qualified Text.PrettyPrint.Boxes as B (Box, left, printBox, text, vcat,
+                                              (<+>))
 
 k :: Integer
 k = 0
@@ -16,22 +21,28 @@ text :: Text
 text =
   "##liet do sta rasti nam biez starosti hod ot hoda rasti našiej bodrosti slavtie molot i stich ziemliu molodosti#"
 
+lengthI :: Text -> Integer
+lengthI = toInteger . length
+
 countText :: Text -> Integer
 countText = count text
 
 count' :: Text -> Double
 count' x =
-  fromIntegral
-    ((countText x + 1) * snC ((toInteger . length) x) (countText x + 1)) /
-  fromIntegral (snC ((toInteger . length) x) (countText x))
+  fromIntegral (countText x + 1) * snC (lengthI x) (countText x + 1) /
+  snC (lengthI x) (countText x)
 
-snC :: Integer -> Integer -> Integer
-snC = nC
+snC :: Integer -> Integer -> Double
+snC len r =
+  let nc = nC len r
+   in if nc > 0
+        then fromIntegral nc
+        else 0.0
 
 nC :: Integer -> Integer -> Integer
 nC 0 _ = 0
 nC len 0 =
-  ((toInteger . length . nub) (DT.unpack text) ^ len) -
+  (toInteger . length . nub) (DT.unpack text) ^ len -
   sum (map (nC len) [1 .. (toInteger . length . nub) (subLists len text)])
 nC len r =
   let sub = nub (subLists len text)
@@ -40,7 +51,7 @@ nC len r =
 
 subLists :: Integer -> Text -> [Text]
 subLists len t =
-  if (toInteger . length) t < len
+  if lengthI t < len
     then []
     else DT.take (fromIntegral len) t : subLists len (DT.tail t)
 
@@ -51,14 +62,34 @@ alpha s wrd =
     [y | y <- nub (DT.unpack text), countText (DT.snoc wrd y) <= k]
 
 beta :: Char -> Text -> Double
-beta _ _ = 1.0
+beta _ wrd = 1.0 - (sum . map (`pBo` wrd))
+    [y | y <- nub (DT.unpack text), countText (DT.snoc wrd y) > k]
 
 pBo :: Char -> Text -> Double
+pBo s "" = count' (DT.singleton s)
 pBo s wrd =
   let wholeWrd = DT.snoc wrd s
    in if countText wholeWrd > k
         then count' wholeWrd / fromIntegral (countText wrd)
-        else 0
+        else alpha s wrd * pBo s (DT.tail wrd)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = B.printBox (makeTable (makePrList 2))
+
+makePrList :: Integer -> [(Text, Double, Double, Double)]
+makePrList len = map makePr (subLists len text)
+
+makePr :: Text -> (Text, Double, Double, Double)
+makePr "" = ("", 0, 0, 0)
+makePr t =
+  ( t
+  , alpha (DT.head t) (DT.tail t)
+  , beta (DT.head t) (DT.tail t)
+  , pBo (DT.head t) (DT.tail t))
+
+makeTable :: (Show a, Show b, Show c, Show d) => [(a, b, c, d)] -> B.Box
+makeTable list = makeRows (unzip4 list)
+  where
+    makeRows (a, b, c, d) =
+      makeColumn a B.<+> makeColumn b B.<+> makeColumn c B.<+> makeColumn d
+    makeColumn l = B.vcat B.left (map (B.text . show) l)
---
+++ test.js
@@ -0,0 +1,101 @@
+#! /usr/bin/node
+
+const k = 0;
+const n = 3;
+const text =
+  "##liet do sta rasti nam biez starosti hod ot hoda rasti našiej bodrosti slavtie molot i stich ziemliu molodosti#";
+const uniqS = [];
+[...text].sort().forEach((s) => {
+  if (!uniqS.includes(s)) uniqS.push(s);
+});
+const textLength = text.length;
+const count = { "": 21 };
+const N_r = [];
+const beta = {};
+const alpha = {};
+N_r[0] = [0];
+
+for (let j = 1; j <= n; j++) {
+  for (let i = 0; i < textLength + 1 - j; i++) {
+    const temp = text.substr(i, j);
+    if (!count[temp]) {
+      count[temp] = 0;
+    }
+    count[temp]++;
+  }
+  N_r[j] = [];
+  N_r[j] = [Math.pow(uniqS.length, j)];
+}
+
+Object.keys(count).forEach((key) => {
+  if (!N_r[key.length][count[key]]) N_r[key.length][count[key]] = 0;
+  N_r[key.length][count[key]]++;
+  N_r[key.length][0]--;
+});
+
+const getCount = (x) => {
+  if (!count[x]) return 0;
+  return count[x];
+};
+
+const getNr = (len, r) => {
+  const nr = N_r[len][r];
+  if (!nr) return 0;
+  return nr;
+};
+
+const getSNr = (len, r) => {
+  return getNr(len, r);
+};
+
+const getCountGT = (x) => {
+  return (
+    ((getCount(x) + 1) * getNr(x.length, getCount(x) + 1)) /
+    getNr(x.length, getCount(x))
+  );
+};
+
+const getD = (x) => {
+  return getCountGT(x) / getCount(x);
+};
+
+const getBeta = (x) => {
+  if (!beta[x])
+    beta[x] =
+      1 -
+      uniqS
+        .filter((s) => getCount(x + s) > k)
+        .reduce((prev, s, index) => {
+          return (getD(x + s) * getCount(x + s)) / getCount(x);
+        }, 0);
+  console.log(`getBeta(${x})=${beta[x]}`);
+  return beta[x];
+};
+
+const getAlpha = (x) => {
+  if (!alpha[x]) {
+    alpha[x] =
+      getBeta(x) /
+      uniqS
+        .filter((s) => getCount(x + s) <= k)
+        .reduce((prev, s, index) => {
+          return getPbo(s, x.substring(1));
+        }, 0);
+  }
+  console.log(`getAlpha(${x})=${alpha[x]}`);
+  return alpha[x];
+};
+
+const getPbo = (s, wrds) => {
+  let pob = 0;
+//   if (!wrds) pob = getCountGT(s) / text.length;
+//   else {
+    if (getCount(wrds + s) > k)
+      pob = (getD(wrds + s) * getCount(wrds + s)) / getCount(wrds);
+    else pob = getAlpha(wrds) * getPbo(s, wrds.substring(1));
+//   }
+  console.log(`getPbo(${s}, ${wrds})=${pob}`);
+  return pob;
+};
+
+console.log(getPbo("m", "ie"));
